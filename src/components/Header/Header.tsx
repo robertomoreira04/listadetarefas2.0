e9017bf4d06a640d67b35a1ecc7be942a0d8c4e7
robertomoreira04 <robertodev04@gmail.com>
2024-06-27 08:54:31 -0300

    Header da aplicação
    Criei o Header da aplicação utilizando Typescript, React, Saas e CSS Modules.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import { StatsCard } from "../StatsCard/StatsCard";
+import styles from "./styles.module.scss";
+
+export const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div>
+          <h1>MyTodo</h1>
+
+          <span>Bem vindo, Roberto!</span>
+        </div>
+
+        <div>
+          <StatsCard title="Total de Tarefas" value={5}/>
+          <StatsCard title="Tarefas Pendentes" value={4}/>
+          <StatsCard title="Tarefas Realizadas" value={1}/>
+        </div>
+      </div>
+    </header>
+  );
+};
---
+++ src/components/StatsCard/StatsCard.tsx
@@ -0,0 +1,15 @@
+import styles from "./styles.module.scss";
+
+interface StatsCardProps {
+  title: string;
+  value: number;
+}
+
+export const StatsCard: React.FC<StatsCardProps> = ({title, value}) => {
+  return (
+    <article className={styles.stats_card}>
+      <h2>{title}</h2>
+      <span>{value}</span>
+    </article>
+  );
+};
